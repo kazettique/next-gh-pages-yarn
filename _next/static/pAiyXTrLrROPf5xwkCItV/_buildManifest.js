4b691cb55f2128d42c0fc553b6bcec229d34ea89
kazettique <42999338+kazettique@users.noreply.github.com>
2021-06-06 14:24:51 +0000

    Deploying to gh-pages from  @ 140267eda93009e25a9dfd5ab7b9a95dde6dc2a0 🚀

---
+++ _next/static/pAiyXTrLrROPf5xwkCItV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5f71468ab31a5b73d12a.js"],"/_error":["static/chunks/pages/_error-d799e3a96772416696b7.js"],"/about":["static/chunks/pages/about-d095bdf7a6e71da9cc61.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
