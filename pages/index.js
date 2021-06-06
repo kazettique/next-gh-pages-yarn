eaf6ac85ea77d7b9ca6e62d1eac474bc0494bd39
Woody <kazettique@gmail.com>
2021-06-06 22:02:39 +0800

    add image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 export default function Home() {
   return (
     <div>
@@ -6,6 +7,7 @@ export default function Home() {
       <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
       </Link>
+      <Image src="/test.png" width={300} height={300} quality={100} />
     </div>
   )
 }
